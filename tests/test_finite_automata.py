8f4c82c59d1f13b8b36a7ea57234d5e75ad34252
André Fernandes <fpf.andre@gmail.com>
2022-09-01 11:03:18 -0300

    Implement a poc of the finite automata

---
+++ tests/test_finite_automata.py
@@ -0,0 +1,32 @@
+from parsero.finite_automata import FiniteAutomata
+from parsero.state import State
+
+
+def test_even_chars():
+    """
+    L = {w | w bellows to {a, b}* and w is even}
+    """
+    pass
+
+    states = [State("q0", True), State("q1", False)]
+
+    transitions = [
+        (0, "a", 1),
+        (0, "b", 1),
+        (1, "a", 0),
+        (1, "b", 0),
+    ]
+
+    template = [
+        ("aa", True),
+        ("aaa", False),
+        ("ab", True),
+        ("aba", False),
+        ("", True),
+        ("c", False),
+    ]
+
+    automata = FiniteAutomata(states=states, transitions=transitions)
+
+    for string, answer in template:
+        assert automata.evaluate(string) == answer
