a5d74206e1455e5af80c39a2dcdc44566e8d6551
Gabriel Holstein Meireles <ghm12@protonmail.com>
2022-11-03 13:24:16 -0300

    Add string support for ContextFree Grammar to allow easier visibility by printing the object

--- parsero/contextfree_grammar.py
+++ parsero/contextfree_grammar.py
@@ -43,10 +43,13 @@ class ContextFreeGrammar:
 
                     new_prod = prod.replace(nullable, "")
 
+                    # If new nullable symbol, place it in the nullable_symbol list and
+                    # recheck every production
+                    # else add the new production if it doesn't exist already
                     if new_prod == "" and not symbol in nullable_symbol:
                         nullable_symbol.append(symbol)
-                        symbols_to_check.append(symbol)
-                    elif not new_prod == "":
+                        [symbols_to_check.append(symbol) for symbol in new_production_rules.keys() if symbol not in symbols_to_check]
+                    elif not new_prod == "" and new_prod not in new_production_rules[symbol]:
                         new_production_rules[symbol].append(new_prod)
 
                 i += 1
@@ -57,10 +60,19 @@ class ContextFreeGrammar:
 
             new_production_rules[self.initial_symbol] = ["{}".format(old_initial_symbol), "&"]
 
-        # remove duplicates
-        for production in new_production_rules.values():
-            for prod in production:
-                while production.count(prod) > 1:
-                    production.remove(prod)
-
         self.production_rules = new_production_rules
+
+    def __str__(self):
+        data = "{} -> ".format(self.initial_symbol)
+
+        data += " | ".join([prod for prod in self.production_rules[self.initial_symbol]]).strip()
+
+        for symbol, production in self.production_rules.items():
+            if symbol == self.initial_symbol:
+                continue
+
+            data += "\n"
+            data += "{} -> ".format(symbol)
+            data += " | ".join([prod for prod in self.production_rules[symbol]]).strip()
+
+        return data
