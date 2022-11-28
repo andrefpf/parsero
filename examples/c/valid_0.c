32babd9bee0b8ccefa59445626f2e643563f9947
André Fernandes <fpf.andre@gmail.com>
2022-11-28 20:41:54 -0300

    add some c grammar

---
+++ examples/c/valid_0.c
@@ -0,0 +1,1 @@
+test = 1 + 1 * (3 + 2 * 1) + "UEEEPA" * test() * test(1,2,3)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,5 +2,5 @@ from parsero.parser import Parser
 
 
 def test_python():
-    parser = Parser("examples/python.regex", "examples/python.grammar")
-    parser.parse("examples/hello_worlds/python.py")
+    parser = Parser("examples/c/c.regex", "examples/c/c.grammar")
+    parser.parse("examples/c/valid_0.c")
