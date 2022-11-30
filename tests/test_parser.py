739613fea1cac5e17a8a89bdd81e2843fdca1e72
André Fernandes <fpf.andre@gmail.com>
2022-11-30 14:09:43 -0300

    update C grammar

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,6 +2,6 @@ from parsero.parser import Parser
 
 
 def test_python():
-    lang = "lisp"
+    lang = "c"
     parser = Parser(f"examples/{lang}/{lang}.regex", f"examples/{lang}/{lang}.ghm")
-    parser.parse(f"examples/{lang}/str8ts.{lang}")
+    parser.parse(f"examples/{lang}/example.{lang}")
